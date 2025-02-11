85672e754e3b22a6c6675f66b667736f372c101b
onyiipaula <etukaonyinye5@gmail.com>
2025-02-11 20:24:36 +0100

    implemented book retrival endpoint

--- api/routes/books.py
+++ api/routes/books.py
@@ -55,6 +55,16 @@ async def update_book(book_id: int, book: Book) -> Book:
         content=db.update_book(book_id, book).model_dump(),
     )
 
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int):
+    book = db.books.get(book_id)
+    if not book:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"detail": "Book not found"}
+        )
+    return book
+
 
 @router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int) -> None:
